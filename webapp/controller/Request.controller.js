ce8586221f976274caf1f1053c928631847c9238
은정 한 <hanjung25@naver.com>
2025-04-11 03:19:51 +0000

    update

--- webapp/controller/Request.controller.js
+++ webapp/controller/Request.controller.js
@@ -1,7 +1,9 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function (Controller) {
-    "use strict";
-    return Controller.extend("project1.controller.Request", {
+        "sap/ui/core/mvc/Controller",
+        "../model/formatter"
+    ], function (Controller, formatter) {
+        "use strict";
+        return Controller.extend("project1.controller.Request", {
+            formatter: formatter
+        });
     });
-});
